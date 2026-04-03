4e2c312ec3e0bd53cb03923187c8c15a552fcfa3
zuhaaermustafa <stsaddu07@gmail.com>
2026-04-04 00:38:56 +0600

    Create Employee.java

---
+++ srp/Employee.java
@@ -0,0 +1,8 @@
+package srp;
+   public class Employee {
+       private String name;
+       private double salary;
+       public Employee(String name, double salary) { this.name = name; this.salary = salary; }
+       public String getName() { return name; }
+       public double getSalary() { return salary; }
+   }
