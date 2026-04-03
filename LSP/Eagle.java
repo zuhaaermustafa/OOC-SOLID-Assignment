16250efbdbbde0c08bd1da8e93dad6714ee9823a
zuhaaermustafa <stsaddu07@gmail.com>
2026-04-04 00:58:05 +0600

    Create Eagle.java

---
+++ LSP/Eagle.java
@@ -0,0 +1,6 @@
+
+package lsp;
+public class Eagle extends Bird implements FlyingBird {
+    public void eat() { System.out.println("Eagle eats."); }
+    public void fly() { System.out.println("Eagle flies."); }
+}
